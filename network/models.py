31a4ac0d90af5dbea7ea56657f4b0e8ed0735136
ricardo-719 <teacherrickyricardo@gmail.com>
2023-03-04 09:15:11 +0700

    Add preliminary models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chirp = models.CharField(max_length=280)
+    likes = models.IntegerField()
+    date = models.DateField()
+
+class Profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    profilePic=models.CharField(max_length=250, null=True, blank=True)
+    introduction=models.CharField(max_length=400)
+
+class Follows(models.Model):
+    follower=models.CharField(max_length=80)
+    followed=models.CharField(max_length=80)
