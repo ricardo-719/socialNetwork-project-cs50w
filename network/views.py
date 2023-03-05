c22a0e2aa4a0d020046677816b4a9801741976cc
ricardo-719 <teacherrickyricardo@gmail.com>
2023-03-05 21:52:13 +0700

    Implement chirp forms

--- network/views.py
+++ network/views.py
@@ -3,12 +3,38 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.forms import ModelForm, Textarea
+from datetime import datetime
 
 from .models import User, Post, Profile, Follows
 
 
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['chirp']
+        """ exclude = ['user', 'likes', 'date'] """
+        widgets = {'chirp': Textarea(attrs={
+            'class': "form-control px-2 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out m-1 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
+            'cols': 16,
+            'rows': 8,
+            'style': 'width: 35%',
+            'autocomplete': "off",
+            'placeholder': 'What\'s Happening?', 
+            })}
+
 def index(request):
-    return render(request, "network/index.html")
+    form = PostForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            chirp = request.POST['chirp']
+            f = Post(user=request.user, chirp=chirp, likes=0, date=datetime.now().strftime("%Y-%m-%d"))
+            f.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/index.html", {
+            "form": PostForm()
+        })
 
 
 def login_view(request):
